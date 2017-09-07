90d549fbc03d00c46f8b04e6be0aa97f6582e75a
Pablo <pdehevia@gmail.com>
2017-09-07 19:53:20 +0200

    Actualización claves servicios y correo

--- TravelAssistant/app/src/main/java/proyect/travelassistant/activitys/IntroActivity.java
+++ TravelAssistant/app/src/main/java/proyect/travelassistant/activitys/IntroActivity.java
@@ -1,12 +1,15 @@
 package proyect.travelassistant.activitys;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 
 import proyect.travelassistant.R;
 import proyect.travelassistant.beans.AuxiliarData;
 import proyect.travelassistant.fragments.FragmentIntro;
+import proyect.travelassistant.utils.RestClient;
 
 public class IntroActivity extends BaseActivity {
 
@@ -35,5 +38,4 @@ public class IntroActivity extends BaseActivity {
     public void onBackPressed() {
         finish();
     }
-
 }
